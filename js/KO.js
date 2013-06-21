050467707a75f739079cebb782821ae9f8ca66bd
Dave Kennedy <davidkennedy85@github.com>
2013-06-21 13:19:59 -0600

    Added updateView function; Changed demo model to use constructor function for testing purposes

--- js/KO.js
+++ js/KO.js
@@ -6,62 +6,61 @@ var KO = (function () {
         if (obj[props[0]] instanceof Object) {
             return getProperty(obj[props[0]], props.slice(1, props.length));
         }
-        
+
         return obj[props[0]];
     }
-    
+
     function setProperty(obj, props, val) {
         if (obj[props[0]] instanceof Object) {
             setProperty(obj[props[0]], props.slice(1, props.length), val);
-
             return;
         }
-        
+
         obj[props[0]] = val;
     }
-    
+
     function createPointer(model, mapping) {
         return function (val) {
             if (arguments.length > 0) {
                 setProperty(model, mapping.split('.'), val);
                 return val;
             }
-            
+
             return getProperty(model, mapping.split('.'));
         };
     }
-    
+
     function setElementValue(el, val) {
         if (el.value === undefined) {
             el.innerHTML = val;
             return;
         }
-        
+
         el.value = val;
     }
-    
+
     function addModelEvents(model, mapping) {
         if (mapping === undefined) {
             mapping = '';
         } else {
             mapping = mapping + '.';
         }
-        
+
         Object.keys(model).forEach(function (key) {
             if (model[key] instanceof Object) {
                 addModelEvents(model[key], mapping + key);
                 return;
             }
-            
+
             var value = model[key];
-            
+
             Object.defineProperty(model, key, {
                 get: function () {
                     return value;
                 },
                 set: function (val) {
                     value = val;
-                    
+
                     window.dispatchEvent(new CustomEvent('modelPropertySet', {
                         detail: {
                             mapping: mapping + key,
@@ -72,18 +71,20 @@ var KO = (function () {
             });
         });
     }
-    
+
     function createMappings(model) {
         var elements = document.getElementsByClassName('bind'),
-            mappings = {};
-        
-        for (var i = 0; i < elements.length; i++) {
-            var mapping = elements[i].dataset.mapping;
-            
+            mappings = {},
+            mapping,
+            i;
+
+        for (i = 0; i < elements.length; i++) {
+            mapping = elements[i].dataset.mapping;
+
             if (mapping === undefined) {
                 break;
             }
-            
+
             if (mappings[mapping] === undefined) {
                 mappings[mapping] = {
                     pointer: createPointer(model, mapping),
@@ -93,36 +94,50 @@ var KO = (function () {
                 mappings[mapping].elements.push(elements[i]);
             }
         }
-        
+
         return mappings;
     }
-    
+
     function addEventListeners(mappings) {
         window.addEventListener('change', function (event) {
             var mapping = event.target.dataset.mapping;
-            
+
             if (mapping === undefined) {
                 return;
             }
-            
+
             mappings[mapping].pointer(event.target.value);
         });
-        
+
         window.addEventListener('modelPropertySet', function (event) {
-            var elements = mappings[event.detail.mapping].elements;
-            
-            for (var i = 0; i < elements.length; i++) {
+            var elements = mappings[event.detail.mapping].elements,
+                i;
+
+            for (i = 0; i < elements.length; i++) {
                 setElementValue(elements[i], event.detail.value);
             }
         });
     }
-    
+
+    function updateView(mappings) {
+        Object.keys(mappings).forEach(function (key) {
+            var elements = mappings[key].elements,
+                i;
+
+            for (i = 0; i < elements.length; i++) {
+                setElementValue(elements[i], mappings[key].pointer());
+            }
+        });
+    }
+
     function bind(model) {
         addModelEvents(model);
-        
+
         var mappings = createMappings(model);
-        
+
         addEventListeners(mappings);
+
+        updateView(mappings);
     }
 
     var Module = {};
